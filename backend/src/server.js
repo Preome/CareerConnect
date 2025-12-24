096fdce8d4661de4d4eff9593c893609d4386229
Preome <tasdidanpreome@gmail.com>
2025-12-24 22:19:31 +0600

    adminroutes in server.js fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,7 @@ const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const jobRoutes = require("./routes/jobRoutes");
 const applicationRoutes = require("./routes/applicationRoutes");
+const adminRoutes = require("./routes/adminRoutes");
 
 
 // new query forum routes
@@ -39,7 +40,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/jobs", jobRoutes);
 app.use("/api/applications", applicationRoutes);
-
+app.use("/api/admin", adminRoutes);
 
 // new Query Forum API
 app.use("/api/query-forum", queryForumRoutes);
